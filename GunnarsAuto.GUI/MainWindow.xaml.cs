8b36e9c8113828df254ba7b393a88a3932cc976e
Nicklas Winther <nick3845@edu.campusvejle.dk>
2020-03-03 13:53:10 +0100

    See list of sales for sales person

--- GunnarsAuto.GUI/MainWindow.xaml.cs
+++ GunnarsAuto.GUI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GunnarsAuto.Entities;
+using GunnarsAuto.GUI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,15 +22,25 @@ namespace GunnarsAuto.GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        SalesPersonViewModel salesPersonViewModel = new SalesPersonViewModel();
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = salesPersonViewModel;
         }
 
         private void CreateCarButton_Click(object sender, RoutedEventArgs e)
         {
-            CreateCarWindow createCarWindow= new CreateCarWindow();
+            CreateCarWindow createCarWindow = new CreateCarWindow();
             createCarWindow.Show();
+        }
+
+        private void SalesPersonsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            salesPersonViewModel.SelectedSalesPerson = SalesPersonsComboBox.SelectedItem as SalesPerson;
+            SalesDataGrid.ItemsSource = salesPersonViewModel.Sales;
+            CreateCarButton.IsEnabled = true;
+            
         }
     }
 }
---
+++ GunnarsAuto.GUI/ViewModels/SalesPersonViewModel.cs
@@ -0,0 +1,58 @@
+﻿using GunnarsAuto.DAL;
+using GunnarsAuto.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GunnarsAuto.GUI.ViewModels
+{
+    class SalesPersonViewModel : INotifyPropertyChanged
+    {
+        private SalesPerson selectedSalesPerson;
+        private List<Sale> sales;
+        private List<SalesPerson> salesPersons;
+
+        public SalesPerson SelectedSalesPerson
+        {
+            get { return selectedSalesPerson; }
+            set
+            {
+                selectedSalesPerson = value;
+                OnPropertyChanged(nameof(SelectedSalesPerson));
+            }
+        }
+        public List<Sale> Sales
+        {
+            get
+            {
+                sales = new SaleRepository().GetSalesBySalesPersonsId(SelectedSalesPerson);
+                return sales;
+            }
+            set
+            {
+                sales = value;
+            }
+        }
+
+        public List<SalesPerson> SalesPersons
+        {
+            get
+            {
+                salesPersons = new SalesPersonRepository().GetAllSalesPersons();
+                return salesPersons;
+            }
+            set
+            {
+                salesPersons = value;
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
